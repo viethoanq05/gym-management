80d5ec6bbd53bda7411bb48137508ac12a928383
viethoanq05 <2351060450@e.tlu.edu.vn>
2026-06-12 15:48:50 +0700

    middleware + trang chủ

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home');
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
+
+
+Route::middleware(['auth', 'role:trainer'])->group(function () {
+    Route::get('/trainer/dashboard', function () {
+        return view('trainer.dashboard');
+    });
+});
+
+Route::middleware(['auth', 'role:member'])->group(function () {
+    Route::get('/member/dashboard', function () {
+        return view('member.dashboard');
+    });
+});
+
+Route::middleware(['auth', 'role:staff'])->group(function () {
+    Route::get('/staff/dashboard', function () {
+        return view('staff.dashboard');
+    });
 });
