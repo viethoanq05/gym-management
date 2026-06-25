210eba82574dfe7fb650058ccf143cc1d65b2bd9
viethoanq05 <2351060450@e.tlu.edu.vn>
2026-06-25 15:17:22 +0700

    update users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,11 +15,9 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->unique();
             $table->enum('role', ['admin', 'staff', 'member', 'trainer']);
-            $table->string('avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
